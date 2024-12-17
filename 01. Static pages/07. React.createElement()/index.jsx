6911e5128d9f6ffb5988dd0296a49b0bc53aa0c9
Luigi Michele Improda <limproda@gmail.com>
2024-12-17 09:04:11 -0500

    Added project 01.07

--- 01. Static pages/07. React.createElement()/index.jsx
+++ 01. Static pages/07. React.createElement()/index.jsx
@@ -1,6 +1,10 @@
 import { createRoot } from "react-dom/client"
+import { createElement } from 'react'
 
 const root = createRoot(document.getElementById("root"))
 root.render(
-    <h1>Hello from React!</h1>
+    <main>
+        <h1>Hello from React!</h1>
+        {createElement('h1', null, 'Another Title')}
+    </main>
 )
