ac6ca290eda59cb2e4ab283147ba533cca568774
Luigi Michele Improda <limproda@gmail.com>
2024-12-21 12:37:17 -0500

    Add folder 24

--- 01. Static pages/24. ReactFacts Project - Main Content Section/components/Main.jsx
+++ 01. Static pages/24. ReactFacts Project - Main Content Section/components/Main.jsx
@@ -8,6 +8,16 @@
 
 export default function Main() {
     return (
-        <h1>Main component here</h1>
+        <main>
+            <h1 className="title">Fun facts about React</h1>
+            <ul className="list">
+                <li>Was first released in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has well over 200K starts on GitHub</li>
+                <li>Is maintained by Meta</li>
+                <li>Powers thousands of enterprise apps, including mobile apps</li>
+            </ul>
+        </main>
+
     )
 }
