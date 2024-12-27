f634c788ebfd0c2c5f758fcac9e627ec699be4d1
Luigi Michele Improda <limproda@gmail.com>
2024-12-26 20:30:15 -0600

    Finish Project 02/03

--- 02. Data-Driven React/03. Travel Journal - Entry Component/components/Entry.jsx
+++ 02. Data-Driven React/03. Travel Journal - Entry Component/components/Entry.jsx
@@ -17,15 +17,22 @@
 export default function Entry(){
     return (
         <article className="journal-entry">
-            <img className="image-entry" src="https://scrimba.com/links/travel-journal-japan-image-url" alt="Mount fuji"/>
-            <div>
-            <img src="../images/marker.png" alt="Marker icon"></img>
-            <p>JAPAN</p>
-            <a href="#">View on Google Maps</a>
-            <h2>Mount Fuji</h2>
-            <p>12 Jan, 2021 - 24 Jan, 2021</p>
-            <p>Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.</p>
+            <div className="main-image-container">
+                <img className="image-entry" src="https://scrimba.com/links/travel-journal-japan-image-url" alt="Mount fuji"/>
             </div>
+            <div className="main-container">
+                <div className="info-container">
+                    <img src="../images/marker.png" alt="Marker icon" className="marker-icon"></img>
+                    <p className="countryName">JAPAN</p>
+                    <a className="mapLink" href="#">View on Google Maps</a>
+                </div>
+                <div>
+                    <h2 className="post-title">Mount Fuji</h2>
+                    <p className="post-date">12 Jan, 2021 - 24 Jan, 2021</p>
+                    <p className="psot-description">Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.</p>
+                </div>
+            </div>
+            
             
         </article>
     )
