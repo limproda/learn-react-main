2b7054d70fc1fcdc53c492d0661cb19e8340e754
Luigi Michele Improda <limproda@gmail.com>
2024-12-29 09:29:25 -0600

    Add projects 02/08 and 02/09

--- 02. Data-Driven React/09. Props Part 4 - Passing data into a component/App.jsx
+++ 02. Data-Driven React/09. Props Part 4 - Passing data into a component/App.jsx
@@ -11,13 +11,30 @@ function App() {
             <Contact
                 img="./images/mr-whiskerson.png"
                 name="Mr. Whiskerson"
+                phone="(212) 555-1234"
+                email="mr.whiskaz@catnap.meow"
             />
-            <Contact />
-            <Contact />
-            <Contact />
+            <Contact
+                img="./images/fluffykins.png"
+                name="Fluffykins"
+                phone="(212) 555-2345"
+                email="f@me.com"
+            />
+            <Contact 
+                img="./images/felix.png"
+                name="Felix"
+                phone="(212) 555-4567"
+                email="thecat@hotmail.com"
+            />
+            <Contact
+                img="./images/pumpkin.png"
+                name="Pumpkin"
+                phone="(0800) CAT KING"
+                email="pumpkin@scrimba.com"
+            />
         </div>
     )
-}
+}3
 
 export default App
 
