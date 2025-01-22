36e56144f07032a6fd43f3a91f0803179ab1cad0
Luigi Michele Improda <limproda@gmail.com>
2025-01-21 18:36:28 -0600

    03.21 to 03.23

--- 03. React State/22. Form basics/index.jsx
+++ 03. React State/22. Form basics/index.jsx
@@ -13,9 +13,13 @@ function App() {
         <label htmlFor="email">Email:</label>
         <input id="email" type="email" name="email" placeholder="joe@schmoe.com" />
         <br />
-        
-        
-        
+
+        <label htmlFor="password">Password:</label>
+        <input id="password" type="password" name="password"/>
+        <br />
+
+        <button>Submit</button>
+
       </form>
     </section>
   )
