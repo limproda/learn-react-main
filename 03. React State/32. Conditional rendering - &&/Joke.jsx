4a276f87fc28d5a3b28fffd79d75dfe2ec42d18b
Luigi Michele Improda <limproda@gmail.com>
2025-01-23 08:22:16 -0600

    First try project 03.31

--- 03. React State/32. Conditional rendering - &&/Joke.jsx
+++ 03. React State/32. Conditional rendering - &&/Joke.jsx
@@ -1,15 +1,20 @@
+import React from "react";
+
 export default function Joke(props) {
     /**
      * Challenge:
      * - Create state `isShown` (boolean, default to `false`)
      * - Add a button that toggles the value back and forth
      */
-    
+    const [isShown, setIsShown] = React.useState(false);
+
     console.log(isShown)
     return (
         <div>
             {props.setup && <h3>{props.setup}</h3>}
-            <p>{props.punchline}</p>
+            
+            {isShow && <p>{props.punchline}</p>}
+            <button onClick={() => setIsShown(prevShown => !prevShown )}>Show punchline</button>
             <hr />
         </div>
     )
