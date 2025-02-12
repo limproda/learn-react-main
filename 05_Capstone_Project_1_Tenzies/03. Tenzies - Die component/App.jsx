0acea224175eb493c7b6edfb4281977e6a85214e
Luigi Michele Improda <limproda@gmail.com>
2025-02-12 08:02:00 -0600

    add 05.04

--- 05_Capstone_Project_1_Tenzies/03. Tenzies - Die component/App.jsx
+++ 05_Capstone_Project_1_Tenzies/03. Tenzies - Die component/App.jsx
@@ -21,11 +21,11 @@ export default function App() {
         <main>
         <div className='container'>
             <Die value= '1'/>
-            <Die value= '1'/>
-            <Die value= '1'/>
-            <Die value= '1'/>
-            <Die value= '1'/>
-            <Die value= '1'/>
+            <Die value= '2'/>
+            <Die value= '3'/>
+            <Die value= '4'/>
+            <Die value= '5'/>
+            <Die value= '6'/>
             <Die value= '1'/>
             <Die value= '1'/>
             <Die value= '1'/>
--- 05_Capstone_Project_1_Tenzies/04. Tenzies - Generate 10 random numbers/App.jsx
+++ 05_Capstone_Project_1_Tenzies/04. Tenzies - Generate 10 random numbers/App.jsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import Die from "./Die"
 
 /**
@@ -8,22 +9,28 @@ import Die from "./Die"
  * 
  * Log the array of numbers to the console for now
  */
-
 export default function App() {
+    const MAX_NUMBER = 6;
+    const [dice, setDice] = React.useState(generateAllNewDice)
+
+    function generateAllNewDice(){
+        return new Array(10)
+        .fill(0)
+        .map(() => Math.ceil(Math.random() * MAX_NUMBER))
+    }
+
+    function rollDice(){
+        setDice(generateAllNewDice)
+    }
+
+    const diceElements = dice.map(num => <Die value={num} />)
+
     return (
         <main>
             <div className="dice-container">
-                <Die value={1} />
-                <Die value={2} />
-                <Die value={3} />
-                <Die value={4} />
-                <Die value={5} />
-                <Die value={6} />
-                <Die value={1} />
-                <Die value={1} />
-                <Die value={1} />
-                <Die value={1} />
+                {diceElements}
             </div>
+            <button className={roll-dice} onClick={rollDice}>Roll</button>
         </main>
     )
 }
